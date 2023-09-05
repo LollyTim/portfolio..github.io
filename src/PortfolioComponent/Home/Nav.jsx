d8e91bbbbe5dbbfba468d94253430e2b85d79252
Lollytim <ololadetimileyin3@gmail.com>
2023-09-05 20:21:11 +0100

    Updtad nav

--- src/PortfolioComponent/Home/Nav.jsx
+++ src/PortfolioComponent/Home/Nav.jsx
@@ -10,15 +10,6 @@ const Nav = () => {
 
   return (
     <div className=' select-none flex justify-between items-center h-16 max-w-6xl mx-auto px-4 first-letter font-outfit text-white bg-[#18102b]'>
-       <div className={!nav ? 'fixed right-0 top-0 w-[50%] h-full border-l border-l-gray-950 bg-[#18102b] mt-16 ease-in-out duration-500 md:hidden' : 'fixed left-[-500px]' }>
-        <ul className=''>
-             <li className='p-4'>Home</li>
-             <li className='p-4 w-34'>About me</li>
-             <li className='p-4'>Service</li>
-             <li className='p-4'>Contact</li>
-             <li className='p-4'>Projects</li>
-        </ul>
-       </div>
        <h1 className='w-full text-3xl font-bold text-[#00df9a] flex whitespace-nowrap' ><span className='text-white'> &lt; L</span>T /&gt;</h1>
        <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
@@ -28,8 +19,17 @@ const Nav = () => {
         <li className='p-4'>Projects</li>
        </ul>
        <div onClick={handleNav} className='block md:hidden'>
-        {!nav ? <AiOutlineClose size={25}/> : <AiOutlineMenu  size={25}/>}
+        {!nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/>}
         
+       </div>
+       <div className={nav ? 'fixed right-0 top-0 w-[50%] h-full border-l border-l-gray-950 bg-[#18102b] mt-16 ease-in-out duration-500 md:hidden ' : 'fixed left-[-500px]' }>
+        <ul className=''>
+             <li className='p-4'>Home</li>
+             <li className='p-4 w-34'>About me</li>
+             <li className='p-4'>Service</li>
+             <li className='p-4'>Contact</li>
+             <li className='p-4'>Projects</li>
+        </ul>
        </div>
 
     </div>
